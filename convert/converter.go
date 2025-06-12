2231d1fb17326e7c064812e9732e22213a9dae68
beck-8 <1504068285@qq.com>
2025-06-12 16:40:53 +0800

    fix https

--- convert/converter.go
+++ convert/converter.go
@@ -556,6 +556,10 @@ func ConvertsV2Ray(buf []byte) ([]map[string]any, error) {
 			socks["password"] = password
 			socks["skip-cert-verify"] = true
 
+			if scheme == "https" {
+				socks["tls"] = true
+			}
+
 			proxies = append(proxies, socks)
 
 		case "anytls":
